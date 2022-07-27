2c50650157efa7d30a0b60bc519029bd27b51c25
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-07-27 13:10:00 +0100

    Routes Setup

--- server.js
+++ server.js
@@ -8,5 +8,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Server is running.'));
 
+// Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
